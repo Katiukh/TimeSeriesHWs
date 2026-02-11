37183e87b88a1aaa9243e563a63509eb3cfff907
Katiukh <34227932+Katiukh@users.noreply.github.com>
2026-02-11 13:36:43 +0900

    Update models.py

--- HW_2/modules/models.py
+++ HW_2/modules/models.py
@@ -293,7 +293,59 @@ class CatBoostDirect(BaseModel):
         - value_col: название столбца с значением ряда.
 
         """
-        # -- Your code here ---
+        for h in range(1, self.horizon + 1):
+        
+            train_features_idx, train_targets_idx = direct_features_targets__train_idx(
+                id_column=train_data[id_col],
+                series_length=len(train_data),
+                horizon_step=h,
+                history_size=self.history,
+            )
+            val_features_idx, val_targets_idx = direct_features_targets__train_idx(
+                id_column=val_data[id_col],
+                series_length=len(val_data),
+                horizon_step=h,
+                history_size=self.history,
+            )
+            # Генерируем признаки и таргеты
+            train_features, train_targets, categorical_features_idx = get_features_df_and_targets(
+                train_data,
+                train_features_idx,
+                train_targets_idx,
+                id_column=id_col,
+                date_column=timestamp_col,
+                target_column=value_col,
+            )
+            val_features, val_targets, _ = get_features_df_and_targets(
+                val_data,
+                val_features_idx,
+                val_targets_idx,
+                id_column=id_col,
+                date_column=timestamp_col,
+                target_column=value_col,
+            )
+            # Инициализируем и обучаем модель CatBoost
+            cb_model = cb.CatBoostRegressor(
+                loss_function="MultiRMSE",
+                random_seed=42,
+                verbose=100,
+                early_stopping_rounds=100,
+                iterations=1500,
+                cat_features=categorical_features_idx,
+            )
+            train_dataset = cb.Pool(
+                data=train_features, label=train_targets, cat_features=categorical_features_idx
+            )
+            eval_dataset = cb.Pool(
+                data=val_features, label=val_targets, cat_features=categorical_features_idx
+            )
+            cb_model.fit(
+                train_dataset,
+                eval_set=eval_dataset,
+                use_best_model=True,
+                plot=False,
+            )
+            self.models.append(cb_model)
 
     def predict(self, test_data, id_col="sensor_id", timestamp_col="timestamp", value_col="value"):
         """Прогнозирование на тестовых данных.
@@ -309,6 +361,32 @@ class CatBoostDirect(BaseModel):
             [id_col, timestamp_col, 'predicted_value'].
 
         """
-        # -- Your code here ---
+    value_col_idx = test_data.columns.get_loc(value_col)
 
-        return test_data[[id_col, timestamp_col, "predicted_value"]].reset_index(drop=True)
+    for h in range(1, self.horizon + 1):
+        test_features_idx, target_features_idx = direct_features__test_idx(
+            id_column=test_data[id_col],
+            series_length=len(test_data),
+            horizon_step=h,
+            history_size=self.history,
+        )
+
+        test_features, _, _ = get_features_df_and_targets(
+            test_data,
+            test_features_idx,
+            target_features_idx,  
+            id_column=id_col,
+            date_column=timestamp_col,
+            target_column=value_col,
+        )
+
+        preds = self.models[h - 1].predict(test_features)
+        preds = np.asarray(preds).reshape(-1, 1)
+
+        test_data.iloc[target_features_idx.flatten(), value_col_idx] = preds[:, 0]
+
+    first_test_date = np.sort(np.unique(test_data[timestamp_col]))[self.history]
+    test_data = test_data[test_data[timestamp_col] >= first_test_date].copy()
+    test_data = test_data.rename(columns={value_col: "predicted_value"})
+
+    return test_data[[id_col, timestamp_col, "predicted_value"]].reset_index(drop=True)
