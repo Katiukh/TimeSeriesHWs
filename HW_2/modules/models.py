af03c9f472a4596b2c6a710dd2499999e2fdc774
Katiukh <34227932+Katiukh@users.noreply.github.com>
2026-03-17 15:16:49 +0900

    Update models.py

--- HW_2/modules/models.py
+++ HW_2/modules/models.py
@@ -5,7 +5,7 @@ from statsforecast import StatsForecast
 from statsforecast.models import Naive
 
 from .feature_generation import get_features_df_and_targets
-from .index_slicing import features__test_idx, features_targets__train_idx, direct_features__test_idx, direct_features_targets__train_idx, direct_mimo_features__test_idx, direct_mimo_features_targets__train_idx
+from .index_slicing import features__test_idx, features_targets__train_idx, direct_features__test_idx, direct_features_targets__train_idx, direct_mimo_features__test_idx, direct_mimo_features_targets__train_idx, direct_mimo_features_targets__holdout_idx
 
 
 class BaseModel:
